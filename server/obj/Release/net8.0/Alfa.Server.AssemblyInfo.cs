2057b81f74f3540888be0caf47e50e0447d3bec7
Sparsh <akinchit.sahoo@gmail.com>
2026-04-12 23:49:44 +0000

    Publish generated build output artifacts (bin/obj)

--- server/obj/Release/net8.0/Alfa.Server.AssemblyInfo.cs
+++ server/obj/Release/net8.0/Alfa.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Alfa.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f1d0e61e87d43540752b01f414beb4d325fb86b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ed09fab89e840c9d9fa2091cfa692557d089f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Alfa.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Alfa.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
